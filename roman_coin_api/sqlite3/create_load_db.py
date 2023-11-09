295c9e3d1c79b74afddf8f8ec55155151b0cb6f5
vbalalian <vbalalian@gmail.com>
2023-11-09 13:42:28 -0800

    wrap connection in with statement

--- roman_coin_api/sqlite3/create_load_db.py
+++ roman_coin_api/sqlite3/create_load_db.py
@@ -4,36 +4,31 @@ import csv
 db_path = './sqlite3/roman_coins.db'
 csv_path = '../web_scraping/roman_coins.csv'
 
-con = sqlite3.connect(db_path)
-cur = con.cursor()
-
 try:
-    cur.execute('''CREATE TABLE IF NOT EXISTS roman_coins (
-                ruler,
-                ruler_detail,
-                id,
-                description,
-                metal,
-                mass,
-                diameter,
-                era,
-                year,
-                inscriptions,
-                txt
-    );''')
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS roman_coins (
+                    ruler,
+                    ruler_detail,
+                    id,
+                    description,
+                    metal,
+                    mass,
+                    diameter,
+                    era,
+                    year,
+                    inscriptions,
+                    txt
+        );''')
     
-    with open(csv_path, 'r') as file:
-        dr = csv.DictReader(file)
+        with open(csv_path, 'r') as file:
+            dr = csv.DictReader(file)
+            for row in dr:
+                cur.execute('''INSERT INTO roman_coins VALUES (:ruler, :ruler_detail, \
+                            :id, :description, :metal, :mass, :diameter, :era, :year, \
+                            :inscriptions, :txt);''', row)
     
-        for row in dr:
-            cur.execute('''INSERT INTO roman_coins VALUES (:ruler, :ruler_detail, \
-                        :id, :description, :metal, :mass, :diameter, :era, :year, \
-                        :inscriptions, :txt);''', row)
-    
-    con.commit()
-
 except sqlite3.Error as e:
     print("SQLite error:", e)
-    con.rollback()
 
 con.close()
