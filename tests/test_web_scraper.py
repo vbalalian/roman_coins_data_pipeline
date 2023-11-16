3dc8359419f0b6b678da4f3d29452845a0104659
vbalalian <vbalalian@gmail.com>
2023-11-16 00:05:43 -0800

    fix test descriptions to match minimum description length

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -68,12 +68,14 @@ def test_pull_subtitle():
     assert pull_subtitle(BeautifulSoup(navigation_text_html, 'html.parser')) is None
 
 # Normal html and soup
-normal_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test Description 1
-</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_123.jpg">Image<
-/a></td></tr><tr><td bgcolor="#C0C0C0">TEST 124</td><td>Test Description 2</td
-><th><a href="TEST_124.txt">Text</a></th><td><a href="TEST_124.jpg">Image</a><
-/td></tr><tr><td bgcolor="#FFD700">TEST 125</td><td>Test Description 3</td><th
-><a href="125.txt">Text</a></th><td><a href="125.jpg">Image</a></td></tr>'''
+normal_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test Description A
+D 350-380 28mm, 8.24g. AVG CAES </td><th><a href="TEST_123.txt">Text</a></th><
+td><a href="TEST_123.jpg">Image</a></td></tr><tr><td bgcolor="#C0C0C0">TEST 12
+4</td><td>Test Description 17 BC AE17mm,  filler. PON TR COS</td><th><a href="
+TEST_124.txt">Text</a></th><td><a href="TEST_124.jpg">Image</a></td></tr><tr><
+td bgcolor="#FFD700">TEST 125</td><td>Test Description 17-109 AD  Filler 8.4 g
+. PON TR P</td><th><a href="125.txt">Text</a></th><td><a href="125.jpg">Image<
+/a></td></tr>'''
 normal_html = normal_html.replace('\n', '')
 normal_soup = BeautifulSoup(normal_html, 'html.parser')
 normal_coins = [coin.contents for coin in normal_soup.find_all('tr') 
@@ -106,16 +108,17 @@ def test_coin_id():
     assert coin_id(normal_coins[2]) == 'TEST 125'
 
     # Case with missing id
-    missing_id_html = '''<tr><td bgcolor="#b87333"></td><td>Test Desc</td><th>
-<a href="123.txt">Text</a></th><td><a href="123.jpg">Image</a></td></tr>'''
+    missing_id_html = '''<tr><td bgcolor="#b87333"></td><td>Test Description A
+D 350-380 28mm, 8.24g. AVG CAES </td><th><a href="123.txt">Text</a></th><td><a
+ href="123.jpg">Image</a></td></tr>'''
     missing_id_coin = coin_from_html(missing_id_html)
     assert coin_id(missing_id_coin) is None
 
 def test_coin_description():
     # Normal case
-    assert coin_description(normal_coins[0]) == 'Test Description 1'
-    assert coin_description(normal_coins[1]) == 'Test Description 2'
-    assert coin_description(normal_coins[2]) == 'Test Description 3'
+    assert coin_description(normal_coins[0]) == 'Test Description AD 350-380 28mm, 8.24g. AVG CAES '
+    assert coin_description(normal_coins[1]) == 'Test Description 17 BC AE17mm,  filler. PON TR COS'
+    assert coin_description(normal_coins[2]) == 'Test Description 17-109 AD  Filler 8.4 g. PON TR P'
 
     # Case with missing description
     missing_desc_html = '''<tr><td bgcolor="#B7A642">TEST 123</td><th><a href=
@@ -138,52 +141,36 @@ def test_coin_metal():
     
 def test_coin_era():
     # Case with era 'AD'
-    era_AD_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test AD 24-36 
-filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_123
-.jpg">Image</a></td></tr>'''
-    era_AD_coin = coin_from_html(era_AD_html)
-    assert coin_era(era_AD_coin) == 'AD'
+    assert coin_era(normal_coins[0]) == 'AD'
     
     # Case with era 'BC'
-    era_BC_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test 248-297 B
-C filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_12
-3.jpg">Image</a></td></tr>'''
-    era_BC_coin = coin_from_html(era_BC_html)
-    assert coin_era(era_BC_coin) == 'BC'
+    assert coin_era(normal_coins[1]) == 'BC'
 
     # Case with no era
-    missing_era_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test 248-
-297 filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_
-123.jpg">Image</a></td></tr>'''
+    missing_era_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test Desc
+ription 217-219 AE17mm, 8.4 g. PON TR P Filler</td><th><a href="TEST_123.txt">
+Text</a></th><td><a href="TEST_123.jpg">Image</a></td></tr>'''
     missing_era_coin = coin_from_html(missing_era_html)
     assert coin_era(missing_era_coin) is None
 
 def test_coin_year():
     # Case with one year
-    single_year_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test 24 A
-D filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_12
-3.jpg">Image</a></td></tr>'''
+    single_year_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test Desc
+ription 224 AD AE17mm, 8.4 g. PON TR P Filler</td><th><a href="TEST_123.txt">T
+ext</a></th><td><a href="TEST_123.jpg">Image</a></td></tr>'''
     single_year_coin = coin_from_html(single_year_html)
-    assert coin_year(single_year_coin) == 24
+    assert coin_year(single_year_coin) == 224
 
     # Case with multiple years
-    multi_years_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test AD 2
-48-297 filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TE
-ST_123.jpg">Image</a></td></tr>'''
-    multi_years_coin = coin_from_html(multi_years_html)
-    assert coin_year(multi_years_coin) == 248
+    assert coin_year(normal_coins[2]) == 17
 
     # Case with BC (negative) years
-    BC_years_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test 49-36 B
-C filler etc.</td><th><a href="TEST_123.txt">Text</a></th><td><a href="TEST_12
-3.jpg">Image</a></td></tr>'''
-    BC_years_coin = coin_from_html(BC_years_html)
-    assert coin_year(BC_years_coin) == -49
+    assert coin_year(normal_coins[1]) == -17
 
     # Case with no years
-    no_years_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test filler 
-etc.</td><td><a href="TEST_123.txt">Text</a></td><td><a href="TEST_123.jpg">
-Image</a></td></tr>'''
+    no_years_html = '''<tr><td bgcolor="#b87333">TEST 123</td><td>Test Descrip
+tion  AE17mm, 8.4 g. PON TR P Filler filler</td><td><a href="TEST_123.txt">Tex
+t</a></td><td><a href="TEST_123.jpg">Image</a></td></tr>'''
     no_years_coin = coin_from_html(no_years_html)
     assert coin_year(no_years_coin) is None
 
@@ -234,23 +221,25 @@ def test_coin_mass():
                  '7.3 g.', '10-51 g.', '(3.80 gm).', '9.59g',]
     for ab in gram_abs:
         value = float(ab.split('g')[0].strip().replace('-', '.').replace('(', ''))
-        ab_html = f'''<tr><td bgcolor="#FF0000">Id</td><td>Test Desc {ab} fill
-er</td><td><a href='123.txt'>txt</a></td><td><a href='123.jpg'>jpg</a></td></tr>'''
+        ab_html = f'''<tr><td bgcolor="#FF0000">Id</td><td>Test Description Fi
+ller filler Filler filler {ab} filler</td><td><a href='123.txt'>txt</a></td><t
+d><a href='123.jpg'>jpg</a></td></tr>'''
         ab_html = ab_html.replace('\n', '')
         ab_coin = coin_from_html(ab_html)
         mass = coin_mass(ab_coin)
         assert mass == value
 
     # Case with no grams
-    assert coin_mass(normal_coins[0]) is None
+    assert coin_mass(normal_coins[1]) is None
 
 def test_coin_diameter():
     diameter_examples = {'28.4 mm':28.4, '29mm':29.0, 'AE28mm':28.0, '17 mm,':17.0, 
                          '18.33mm':18.33, '25.13mm,':25.13, '33 mm.':33.0}
     for ex in diameter_examples:
         value = diameter_examples[ex]
-        ex_html = f'''<tr><td bgcolor="#FF0000">Id</td><td>Test Desc {ex} fill
-er</td><td><a href='123.txt'>txt</a></td><td><a href='123.jpg'>jpg</a></td></tr>'''
+        ex_html = f'''<tr><td bgcolor="#FF0000">Id</td><td>Test Description Fi
+ller filler Filler filler {ex} filler</td><td><a href='123.txt'>txt</a></td><t
+d><a href='123.jpg'>jpg</a></td></tr>'''
         ex_html = ex_html.replace('\n', '')
         ex_coin = coin_from_html(ex_html)
         diameter = coin_diameter(ex_coin)
@@ -258,7 +247,7 @@ er</td><td><a href='123.txt'>txt</a></td><td><a href='123.jpg'>jpg</a></td></tr>
         assert diameter == value
 
     # Case with no grams
-    assert coin_diameter(normal_coins[0]) is None
+    assert coin_diameter(normal_coins[2]) is None
  
 # def test_coin_inscriptions():
 # def test_coin_df():
