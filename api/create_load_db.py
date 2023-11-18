63cf4c48119891807da3a5f37f8fcbf34696b26d
vbalalian <vbalalian@gmail.com>
2023-11-17 17:44:36 -0800

    break main into table and path agnostic functions

--- api/create_load_db.py
+++ api/create_load_db.py
@@ -3,38 +3,55 @@ import csv
 
 db_path = 'api/roman_coins.db'
 csv_path = 'web_scraping/roman_coins.csv'
+table_name = 'roman_coins'
+table_columns = ['ruler', 'ruler_detail', 'id', 'description', 'metal', 
+                 'mass', 'diameter', 'era', 'year', 'inscriptions', 'txt']
+column_dtypes = ['TEXT', 'TEXT', 'TEXT PRIMARY KEY', 'TEXT', 'TEXT', 'REAL', 
+                 'REAL', 'TEXT', 'INTEGER', 'TEXT', 'TEXT']
 
-def main(db_path = db_path):
+def connect_db(path:str):
+    '''Returns a connection with SQLite db at path'''
     try:
-        with sqlite3.connect(db_path) as con:
-            cur = con.cursor()
-            cur.execute('''CREATE TABLE IF NOT EXISTS roman_coins (
-                        ruler TEXT,
-                        ruler_detail TEXT,
-                        id TEXT PRIMARY KEY,
-                        description TEXT,
-                        metal TEXT,
-                        mass REAL,
-                        diameter REAL,
-                        era TEXT,
-                        year INTEGER,
-                        inscriptions TEXT,
-                        txt TEXT
-            );''')
-        
-            with open(csv_path, 'r') as file:
-                dr = csv.DictReader(file)
-                for row in dr:
-                    cur.execute('''INSERT INTO roman_coins VALUES (:ruler, :ruler_detail, \
-                                :id, :description, :metal, :mass, :diameter, :era, :year, \
-                                :inscriptions, :txt);''', row)
-        
+        con = sqlite3.connect(path)
+        con.row_factory = sqlite3.Row
+        return con
     except sqlite3.Error as e:
-        print("SQLite error:", e)
+        print("SQLite error (connect_db):", e)
 
-    finally:
-        if con:
-            con.close()
+def csv_rows(path:str):
+    '''Reads CSV file at path, returns a list containing each row as a dictionary.'''
+    with open(path, 'r') as file:
+        dr = csv.DictReader(file)
+        return list(dr)
+
+def create_table(path:str, table:str, cols:list, dtypes:list):
+    '''Creates a table based on input connection & parameters'''
+    with connect_db(path) as con:
+        try:
+            cur = con.cursor()
+            cur.execute(f'CREATE TABLE IF NOT EXISTS {table} (' + 
+                        ', '.join(f'{col} {dtype}' for col, dtype 
+                                 in zip(cols, dtypes)) + ');')
+        except sqlite3.Error as e:
+            print("SQLite error (create_table):", e)
+
+def load_table(path:str, table:str, rows:list[dict]):
+    '''Loads table columns with values'''
+    with connect_db(path) as con:
+        try:
+            cur = con.cursor()
+            for row in rows:
+                columns = ', '.join(row.keys())
+                placeholders = ', '.join(f':{col}' for col in row.keys())
+                query = f'INSERT INTO {table} ({columns}) VALUES ({placeholders});'
+                cur.execute(query, row)
+        except sqlite3.Error as e:
+            print("SQLite error (load_table):", e)
+
+def main(path:str, table:str, columns:list, dtypes:list, rows:list[dict]):
+    create_table(path, table, columns, dtypes)
+    load_table(path, table, rows)
 
 if __name__ == '__main__':
-    main()
+    roman_coin_rows = csv_rows(csv_path)
+    main(db_path, table_name, table_columns, column_dtypes, roman_coin_rows)
