8677def0c3d732ab83ccb5567d842cb9e113df29
vbalalian <vbalalian@gmail.com>
2023-11-20 15:00:39 -0800

    Add db connection management closes #24, Temporarily remove async functionality from endpoints, closes #29

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query, HTTPException, Depends
 from typing import Annotated
 import sqlite3
 
@@ -6,13 +6,16 @@ db_path = 'api/roman_coins.db'
 
 app = FastAPI()
 
-def connect_db():
-    con = sqlite3.connect(db_path)
-    con.row_factory = sqlite3.Row
-    return con
+def get_db():
+    db = sqlite3.connect(db_path)
+    db.row_factory = sqlite3.Row
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get('/')
-async def root():
+def root():
     return {
         'message':'Welcome to the Roman Coin Data API',
         'status':'OK',
@@ -21,7 +24,7 @@ async def root():
         }
 
 @app.get('/v1/')
-async def v1_root():
+def v1_root():
     return {
         'message':'Welcome to the Roman Coin Data API - Version 1',
         'status':'OK',
@@ -34,7 +37,8 @@ async def v1_root():
         }
 
 @app.get('/v1/coins/')
-async def read_coins(
+def read_coins(
+    db: sqlite3.Connection = Depends(get_db), 
     page: int = 1, 
     page_size: int = 10, 
     sort_by: str = None,
@@ -44,66 +48,65 @@ async def read_coins(
     year: int = None
     ):
 
-    with connect_db() as con:
-        cur = con.cursor()
+    cur = db.cursor()
 
-        # Base query
-        query = 'SELECT * FROM roman_coins'
-        params = []
+    # Base query
+    query = 'SELECT * FROM roman_coins'
+    params = []
 
-        # Filtering logic
-        conditions = []
-        if ruler:
-            conditions.append('ruler = ?')
-            params.append(ruler)
-        if metal:
-            conditions.append('metal = ?')
-            params.append(metal)
-        if era:
-            conditions.append('era = ?')
-            params.append(era)
-        if year:
-            conditions.append('year = ?')
-            params.append(year)
-        if conditions:
-            query += ' WHERE ' + ' AND '.join(conditions)
-        
-        # Sorting logic
-        if sort_by:
-            query += f' ORDER BY {sort_by}'
+    # Filtering logic
+    conditions = []
+    if ruler:
+        conditions.append('ruler = ?')
+        params.append(ruler)
+    if metal:
+        conditions.append('metal = ?')
+        params.append(metal)
+    if era:
+        conditions.append('era = ?')
+        params.append(era)
+    if year:
+        conditions.append('year = ?')
+        params.append(year)
+    if conditions:
+        query += ' WHERE ' + ' AND '.join(conditions)
+    
+    # Sorting logic
+    if sort_by:
+        query += f' ORDER BY {sort_by}'
 
-        # Pagination logic
-        query += ' LIMIT ? OFFSET ?'
-        params += [page_size, (page - 1) * page_size]
+    # Pagination logic
+    query += ' LIMIT ? OFFSET ?'
+    params += [page_size, (page - 1) * page_size]
 
-        # Execute query
-        cur.execute(query, params)
-        coins = cur.fetchall()
+    # Execute query
+    cur.execute(query, params)
+    coins = cur.fetchall()
 
-    con.close()
     return [dict(row) for row in coins]
 
 @app.get('/v1/coins/search')
-async def search_coins(query: Annotated[str | None, Query(min_length=1, max_length=50)]):
+def search_coins(
+    query: Annotated[str | None, Query(min_length=1, max_length=50)], 
+    db: sqlite3.Connection = Depends(get_db)
+    ):
 
     if query:
-        with connect_db() as con:
-            cur = con.cursor()
-            cur.execute('SELECT * FROM roman_coins WHERE description LIKE ?', ('%' + query + '%',))
-            search_result = cur.fetchall()
-        con.close()
+        cur = db.cursor()
+        cur.execute('SELECT * FROM roman_coins WHERE description LIKE ?', ('%' + query + '%',))
+        search_result = cur.fetchall()
         return [dict(row) for row in search_result]
     
     raise HTTPException(status_code=400, detail='Query string is empty')
 
 @app.get('/v1/coins/id/{coin_id}')
-async def coin_by_id(coin_id: str):
+def coin_by_id(coin_id: str, db: sqlite3.Connection = Depends(get_db)):
 
-    with sqlite3.connect(db_path) as con:
-        cur = con.cursor()
-        cur.execute('SELECT * FROM roman_coins WHERE id = ?', (coin_id,))
-        coin = cur.fetchone()
-    con.close()
-    if coin is None:
-        raise HTTPException(status_code=404, detail='Coin not found')
-    return dict(coin)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM roman_coins WHERE id = ?', (coin_id,))
+    coin = cur.fetchone()
+    if coin:
+        return dict(coin)
+    
+    raise HTTPException(status_code=404, detail='Coin not found')
+    
