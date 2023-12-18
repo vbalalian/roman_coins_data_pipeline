24618711a6605c071a8a530afaeed188e02851d7
vbalalian <vbalalian@gmail.com>
2023-12-17 22:02:32 -0800

    add tests: test_add_coin

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -7,6 +7,7 @@ import pytest
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import json
+from datetime import datetime
 
 @pytest.fixture(scope='module')
 def test_client():
@@ -108,104 +109,167 @@ def test_v1_root(test_client):
 def test_read_coins(test_client, test_database):
 
     # Default parameters
-    default_response = test_client.get("/v1/coins/")
-    assert default_response.status_code == 200
-    assert len(default_response.json()["data"]) == 10
-    assert default_response.json()["pagination"] == {"total_items":20, "total_pages":2, "current_page":1, "items_per_page":10}
+    response = test_client.get("/v1/coins/")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 10
+    assert response.json()["pagination"] == {"total_items":20, "total_pages":2, "current_page":1, "items_per_page":10}
     
     # Pagination
-    page_response = test_client.get("/v1/coins/?page=2&page_size=8")
-    assert page_response.status_code == 200
-    assert len(page_response.json()["data"]) == 8
-    assert page_response.json()["pagination"] == {"total_items":20, "total_pages":3, "current_page":2, "items_per_page":8}
+    response = test_client.get("/v1/coins/?page=2&page_size=8")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 8
+    assert response.json()["pagination"] == {"total_items":20, "total_pages":3, "current_page":2, "items_per_page":8}
     
     # Sorting
-    sort_response_1 = test_client.get("/v1/coins/?sort_by=ruler&desc=True")
-    assert sort_response_1.status_code == 200
-    assert len(sort_response_1.json()["data"]) == 10
-    assert sort_response_1.json()["data"][0]["ruler"] == "Hadrian"
+    response = test_client.get("/v1/coins/?sort_by=ruler&desc=True")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 10
+    assert response.json()["data"][0]["ruler"] == "Hadrian"
 
-    sort_response_2 = test_client.get("/v1/coins/?sort_by=year")
-    assert sort_response_2.status_code == 200
-    assert len(sort_response_2.json()["data"]) == 10
-    assert sort_response_2.json()["data"][0]["year"] == 378
+    response = test_client.get("/v1/coins/?sort_by=year")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 10
+    assert response.json()["data"][0]["year"] == 378
 
-    sort_response_3 = test_client.get("/v1/coins/?sort_by=description")
-    assert sort_response_3.status_code == 400
+    response = test_client.get("/v1/coins/?sort_by=description")
+    assert response.status_code == 400
 
     # Filtering
-    filter_response_1 = test_client.get(r"/v1/coins/?ruler=aelia%20Flaccilla&min_diameter=21")
-    assert filter_response_1.status_code == 200
-    assert len(filter_response_1.json()["data"]) == 5
+    response = test_client.get(r"/v1/coins/?ruler=aelia%20Flaccilla&min_diameter=21")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 5
 
-    filter_response_2 = test_client.get(r"/v1/coins/?metal=gold&max_mass=2")
-    assert filter_response_2.status_code == 200
-    assert len(filter_response_2.json()["data"]) == 3
-    assert filter_response_2.json()["data"][0]["metal"] == "Gold"
+    response = test_client.get(r"/v1/coins/?metal=gold&max_mass=2")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 3
+    assert response.json()["data"][0]["metal"] == "Gold"
 
-    filter_response_3 = test_client.get(r"/v1/coins/?min_year=100&max_year=400")
-    assert filter_response_3.status_code == 200
-    assert len(filter_response_3.json()["data"]) == 5
+    response = test_client.get(r"/v1/coins/?min_year=100&max_year=400")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 5
 
-    filter_response_4 = test_client.get(r"/v1/coins/?era=ad")
-    assert filter_response_4.status_code == 200
-    assert len(filter_response_4.json()["data"]) == 9
-    assert filter_response_4.json()["data"][0]["era"] == "AD"
+    response = test_client.get(r"/v1/coins/?era=ad")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 9
+    assert response.json()["data"][0]["era"] == "AD"
 
-    filter_response_5 = test_client.get(r"/v1/coins/?start_created=2023-12-11T07:55:00")
-    assert filter_response_5.status_code == 200
-    assert len(filter_response_5.json()["data"]) == 1
+    response = test_client.get(r"/v1/coins/?start_created=2023-12-11T07:55:00")
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 1
 
-    filter_response_6 = test_client.get(r"/v1/coins/?min_diameter=30")
-    assert filter_response_6.status_code == 400
+    response = test_client.get(r"/v1/coins/?min_diameter=30")
+    assert response.status_code == 400
 
 # Coin Search endpoint
 def test_search_coins(test_client, test_database):
     
     # Query with multiple results
-    search_response_1 = test_client.get(r"/v1/coins/search?query=Constantinople")
-    assert search_response_1.status_code ==200
-    assert len(search_response_1.json()) == 8
+    response = test_client.get(r"/v1/coins/search?query=Constantinople")
+    assert response.status_code ==200
+    assert len(response.json()) == 8
 
     # Query with one result
-    search_response_2 = test_client.get(r"/v1/coins/search?query=Hadrian")
-    assert search_response_2.status_code ==200
-    assert len(search_response_2.json()) == 1
-    assert search_response_2.json()[0]["ruler"] == "Hadrian"
+    response = test_client.get(r"/v1/coins/search?query=Hadrian")
+    assert response.status_code ==200
+    assert len(response.json()) == 1
+    assert response.json()[0]["ruler"] == "Hadrian"
 
     # Query with no results
-    search_response_3 = test_client.get(r"/v1/coins/search?query=Caligula")
-    assert search_response_3.status_code == 400
-    assert search_response_3.json()["detail"]== "No matching coins found"
+    response = test_client.get(r"/v1/coins/search?query=Caligula")
+    assert response.status_code == 400
+    assert response.json()["detail"]== "No matching coins found"
 
     # Empty query
-    search_response_4 = test_client.get(r"/v1/coins/search?query=")
-    assert search_response_4.status_code == 422
-    assert search_response_4.json()["detail"][0]["type"] == "string_too_short"
+    response = test_client.get(r"/v1/coins/search?query=")
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "string_too_short"
 
     # Multiple queries
-    search_response_5 = test_client.get(r"/v1/coins/search?query=Victory&query=officina")
-    assert search_response_5.status_code == 200
-    assert len(search_response_5.json()) == 2
+    response = test_client.get(r"/v1/coins/search?query=Victory&query=officina")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
 
 # Coins by ID endpoint
 def test_coin_by_id(test_client, test_database):
 
     # Valid ID
-    id_response_1 = test_client.get(r"/v1/coins/id/343a3001-ae2e-4745-888e-994374e398a3")
-    assert id_response_1.status_code ==200
-    assert id_response_1.json()["ruler"] == "Aelia Ariadne"
+    response = test_client.get(r"/v1/coins/id/343a3001-ae2e-4745-888e-994374e398a3")
+    assert response.status_code ==200
+    assert response.json()["ruler"] == "Aelia Ariadne"
     
     # Non-existent ID
-    id_response_2 = test_client.get(r"/v1/coins/id/023-450938fgldf-to0r90ftu-438537")
-    assert id_response_2.status_code == 404
-    assert id_response_2.json()["detail"] == "Coin not found"
+    response = test_client.get(r"/v1/coins/id/023-450938fgldf-to0r90ftu-438537")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Coin not found"
 
     # Invalid ID
-    id_response_3 = test_client.get(r"/v1/coins/id/023450938")
-    assert id_response_3.status_code == 422
-    assert id_response_3.json()["detail"][0]["type"] == "string_too_short"
+    response = test_client.get(r"/v1/coins/id/023450938")
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "string_too_short"
 
     # Empty ID
-    id_response_4 = test_client.get(r"/v1/coins/id/")
-    assert id_response_4.status_code == 404
+    response = test_client.get(r"/v1/coins/id/")
+    assert response.status_code == 404
+
+# Add coin endpoint
+def test_add_coin(test_client, test_database):
+
+    # Normal case, no missing fields
+    coin = {"ruler":"Test Ruler 1", "ruler_detail":"Test Ruler Detail", 
+            "catalog":"Test Catalog", 
+            "description":"This is a test description, minimum 50 characters. ",
+            "metal":"Gold", "mass":8.9, "diameter":20.5, "era":"AD", "year":79,
+            "inscriptions":"AVG,CAES", "txt":"test_file.txt"}
+    test_id = "3049823-d90njfa09-joand09"
+    response = test_client.post(f"/v1/coins/id/{test_id}", json=coin)
+    assert response.status_code == 201
+    assert response.json()["message"] == "Coin added successfully"
+    # Verify coin added to test database
+    response = test_client.get(f"/v1/coins/id/{test_id}")
+    assert response.status_code == 200
+    assert response.json()["ruler"] == "Test Ruler 1"
+    created_at = datetime.strptime(response.json()["created"], r"%Y-%m-%dT%H:%M:%S.%f")
+    created_truncated = created_at.replace(second=0, microsecond=0)
+    current_datetime_truncated = datetime.now().replace(second=0, microsecond=0)
+    assert created_truncated == current_datetime_truncated
+    modified_at = datetime.strptime(response.json()["modified"], r"%Y-%m-%dT%H:%M:%S.%f")
+    modified_truncated = modified_at.replace(second=0, microsecond=0)
+    current_datetime_truncated = datetime.now().replace(second=0, microsecond=0)
+    assert modified_truncated == current_datetime_truncated
+
+    # Normal case, missing fields
+    coin = {"ruler":"Test Ruler 2", "catalog":"Test Catalog", "metal":"Gold"}
+    test_id = "34fn59073bn5-dfpanc34bn9"
+    response = test_client.post(f"/v1/coins/id/{test_id}", json=coin)
+    assert response.status_code == 201
+    assert response.json()["message"] == "Coin added successfully"
+
+    # Verify coin added to test database
+    response = test_client.get(f"/v1/coins/id/{test_id}")
+    assert response.status_code == 200
+    assert response.json()["ruler"] == "Test Ruler 2"
+    created_at = datetime.strptime(response.json()["created"], r"%Y-%m-%dT%H:%M:%S.%f")
+    created_truncated = created_at.replace(second=0, microsecond=0)
+    current_datetime_truncated = datetime.now().replace(second=0, microsecond=0)
+    assert created_truncated == current_datetime_truncated
+    modified_at = datetime.strptime(response.json()["modified"], r"%Y-%m-%dT%H:%M:%S.%f")
+    modified_truncated = modified_at.replace(second=0, microsecond=0)
+    current_datetime_truncated = datetime.now().replace(second=0, microsecond=0)
+    assert modified_truncated == current_datetime_truncated
+
+    # Case with invalid data
+    coin = {"ruler":"Test Ruler 2", "catalog":5, "metal":"Aluminum", "mass":"heavy"}
+    test_id = "akhvuy409-ln4v98439-5c9"
+    response = test_client.post(f"/v1/coins/id/{test_id}", json=coin)
+    assert response.status_code == 422
+
+    # Case with missing ID
+    coin = {"ruler":"Test Ruler 2", "catalog":"Test Catalog", "metal":"Gold"}
+    response = test_client.post("/v1/coins/id/", json=coin)
+    assert response.status_code == 404
+
+    # Case with duplicate ID
+    coin = {"ruler":"Test Ruler 2", "catalog":"Test Catalog", "metal":"Gold"}
+    test_id = "34fn59073bn5-dfpanc34bn9"
+    response = test_client.post(f"/v1/coins/id/{test_id}", json=coin)
+    assert response.status_code == 400
